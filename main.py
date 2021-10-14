89b797b6055485bce1b6d0d603074771e1f10e13
matiasdepino <matudepino@hotmail.com>
2021-10-14 11:33:39 +0200

    Update Main_functions.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 # Import a library of functions called 'pygame'
+
 import pygame
 from main_functions import *
 
@@ -7,4 +8,10 @@ pygame.init()
 
 #call main routine
 size = [1300, 600] # Define size of windows
-main2(size)
+width = int(input("Ancho de la ventana:"))
+height = int(input("Alto de la ventana:"))
+size[0] = width
+size[1] = height
+color = BLUE, RED, GREEN (255, 255, 255)
+title = input("Asignar titulo:")
+main2(size, title, color)
